aa4792aaa256b4c524eb0e18e69f1acf167f2508
David Reichman <davidreichman@MacBook-Pro-Davidr.local>
2025-11-18 22:24:58 +0200

    assignment completed

--- LoanCalc.java
+++ LoanCalc.java
@@ -27,8 +27,9 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
+		double perPeriodRate = rate / 100.0 / 12.0;
 		for (int i=0; i<n; i++){
-			loan = loan * (1 + rate / 100.0) - payment;
+			loan = loan * (1 + perPeriodRate) - payment;
 		}
 		return loan;
 	}
